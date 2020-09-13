63b9259387f6898614f6788876597f65d7932905
Johann Montfort <jmonfu@gmail.com>
2020-09-13 11:41:50 +0200

    hiding and showing the ratings

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Card = ({ cardInfo, onClick }) => {
+const Card = ({ cardInfo, onClick, showRatings }) => {
 
   const evenRatingRows = cardInfo.ratings.reduce(function (rows, key, index) { 
     return (index % 2 === 0 ? rows.push([key]) 
@@ -19,9 +19,19 @@ const Card = ({ cardInfo, onClick }) => {
             evenRatingRows.map(row => ( 
             <div className="row" key={row[0].title}>
               <div className="col-md-4 text-left">{row[0].title}</div>
-              <div className="col-md-2 text-left card-rating-color" onClick={() => onClick({player: cardInfo.player, title: row[0].title, rating: row[0].rating})} >{row[0].rating}</div>
+              { 
+                showRatings ? 
+                <div className="col-md-2 text-left card-rating-color" onClick={() => onClick({player: cardInfo.player, title: row[0].title, rating: row[0].rating})} >{row[0].rating}</div>
+                  : 
+                <div className="col-md-2 text-left" >??</div>
+              }
               <div className="col-md-4 text-left">{row[1].title}</div>
-              <div className="col-md-2 text-left card-rating-color" onClick={() => onClick({player: cardInfo.player, title: row[1].title, rating: row[1].rating})}>{row[1].rating}</div>
+              {
+                showRatings ?
+                <div className="col-md-2 text-left card-rating-color" onClick={() => onClick({player: cardInfo.player, title: row[1].title, rating: row[1].rating})}>{row[1].rating}</div>
+                  : 
+                <div className="col-md-2 text-left" >??</div>
+              }
             </div>
             ))
           }          
--- src/components/Cards/CardsBoard.js
+++ src/components/Cards/CardsBoard.js
@@ -1,48 +1,76 @@
-import React, {useState} from "react"
-import Card from './Card';
+import React, { useState, useEffect } from "react"
+import Card from "./Card"
 
-import joker from '../../assets/joker.png';
-import goalie from '../../assets/goalie.png';
-import defender from '../../assets/defender.png';
-import midfielder from '../../assets/midfielder.png';
-import attacker from '../../assets/attacker.png';
+import joker from "../../assets/joker.png"
+import goalie from "../../assets/goalie.png"
+import defender from "../../assets/defender.png"
+import midfielder from "../../assets/midfielder.png"
+import attacker from "../../assets/attacker.png"
 
 const CardsBoard = () => {
+  const [ratingObj, setRatingObj] = useState({});
+  const [visible1, setVisible1 ] = useState(false);
+  const [visible2, setVisible2 ] = useState(false);
 
-const [ ratingObj, setRatingObj ] = useState({});
-console.log(ratingObj);
+  useEffect(() => {
+    doProcessing()
+  }, [ratingObj])
 
-const cardInfo1 = {
-  player: 1,
-  image: goalie, 
-  title: "Goalkeeper", 
-  ratings: [
-    {title: "Handling", rating: "99"},
-    {title: "Reflexes", rating: "99"},
-    {title: "Defending", rating: "99"},
-    {title: "Strength", rating: "99"},
-    {title: "Passing", rating: "99"},
-    {title: "Flair", rating: "99"},
-    {title: "Finishing", rating: "99"},
-    {title: "Composure", rating: "99"},
-  ]
-}
+  const cardInfo1 = {
+    player: 1,
+    image: goalie,
+    title: "Goalkeeper",
+    ratings: [
+      { title: "Handling", rating: "99" },
+      { title: "Reflexes", rating: "99" },
+      { title: "Defending", rating: "99" },
+      { title: "Strength", rating: "99" },
+      { title: "Passing", rating: "99" },
+      { title: "Flair", rating: "99" },
+      { title: "Finishing", rating: "99" },
+      { title: "Composure", rating: "99" },
+    ],
+  }
 
-const cardInfo2 = {
-  player: 2,
-  image: defender, 
-  title: "Defender",
-  ratings: [
-    {title: "Handling", rating: "80"},
-    {title: "Reflexes", rating: "80"},
-    {title: "Defending", rating: "80"},
-    {title: "Strength", rating: "80"},
-    {title: "Passing", rating: "80"},
-    {title: "Flair", rating: "80"},
-    {title: "Finishing", rating: "80"},
-    {title: "Composure", rating: "80"},
-  ]
-}
+  const cardInfo2 = {
+    player: 2,
+    image: defender,
+    title: "Defender",
+    ratings: [
+      { title: "Handling", rating: "80" },
+      { title: "Reflexes", rating: "80" },
+      { title: "Defending", rating: "80" },
+      { title: "Strength", rating: "80" },
+      { title: "Passing", rating: "80" },
+      { title: "Flair", rating: "80" },
+      { title: "Finishing", rating: "80" },
+      { title: "Composure", rating: "80" },
+    ],
+  }
+
+  const doProcessing = () => {
+    const {player, title, rating} = ratingObj;
+
+    if (player === 1){
+      // reveal opp card
+      // get the same value id
+      // deduct the value
+      // if value is +ve then holding player wins (gets opponent card)
+      // if value is -ve opponent wins (looses card to opponent)
+      // if draw opp wins (looses card to opponent)
+      // Add or subtract the card counter for both players
+    };
+  }
+
+  const startGame = () => {
+    setVisible1(true);
+    setVisible2(false);
+    //generate the card’s values
+    //distribute 26 cards at random when the game start
+    //queue the first card to Player 1
+    //queue the first card to Player 2
+    //unMask ratings of Player 1
+  }
 
   return (
     <div className="container-fluid justify-content-center">
@@ -51,16 +79,31 @@ const cardInfo2 = {
           <div>
             <h4>Player 1</h4>
           </div>
-          <Card cardInfo={cardInfo1} onClick={ratingObj => setRatingObj(ratingObj)} />
+          <Card
+            cardInfo={cardInfo1}
+            showRatings={visible1}
+            onClick={ratingObj => setRatingObj(ratingObj)}
+          />
         </div>
         <div className="col-md-6">
           <h4>Player 2</h4>
-          <Card cardInfo={cardInfo2} onClick={ratingObj => setRatingObj(ratingObj)}/>
+          <Card
+            cardInfo={cardInfo2}
+            showRatings={visible2}
+            onClick={ratingObj => setRatingObj(ratingObj)}
+          />
         </div>
       </div>
       <div className="row top-buffer">
         <div className="col-md-12 text-center">
-          <button id="startGameBtn" name="StartGameButton" className="btn btn-secondary">START GAME</button> 
+          <button
+            id="startGameBtn"
+            name="StartGameButton"
+            className="btn btn-secondary"
+            onClick={() => startGame()}
+          >
+            START GAME
+          </button>
         </div>
       </div>
     </div>
